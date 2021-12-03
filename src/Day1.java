ff1c5b2ebc465e7d77d885ebc0b2353cc599c712
t <loim988@gmail.com>
2021-12-02 20:43:26 -0700

    setting up file reading and compare depth method

--- src/Day1.java
+++ src/Day1.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+
 public class Day1 {
 
+  BufferedReader depthData = BufferedReader(new FileReader("./data/Day1.txt"));
+  int currentDepth;
+  int
+
+  private void compareDepths() {
+    int nextDepth = getNextDepth();
+
+  }
+
+  private void getNextDepth() {
+    currentDepth = String.toInt(depthData.readLine());
+  }
 
   public static void main(String[] args) {
 
