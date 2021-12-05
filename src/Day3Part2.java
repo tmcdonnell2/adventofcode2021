c801805e319dff1c5631c1612ecde9fb794999c0
t <loim988@gmail.com>
2021-12-04 19:04:45 -0700

    day3part2 soln

--- src/Day3Part2.java
+++ src/Day3Part2.java
@@ -1,51 +1,77 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Day3Part2 {
-    int gammaRate = 0;
-    int epsilonRate = 0;
+    int oxygenRating;
+    int scrubbingRating;
 
     List<String> binaryData;
-    int[] amountSet = new int[12];
 
     Day3Part2(List<String> binaryData) {
         this.binaryData = binaryData;
     }
 
     public void solve() {
-        processBinaryData();
-        setRates();
+        oxygenRating = Integer.parseInt(findO2Rating(),2);
+        scrubbingRating = Integer.parseInt(findCO2ScrubbingRating(), 2);
     }
 
-    private void processBinaryData() {
-        for (String line : binaryData) {
-            for (int i = 0; i < line.length(); i++)  {
-                if (line.charAt(i) == '1') {
-                    amountSet[line.length() - 1 - i]++;
-                }
-            }
+    private int getLifeSupportRating() {
+        return oxygenRating * scrubbingRating;
+    }
+
+    private List<String> getCommonListOnPredicate(int index, BiPredicate<List, List> pred, List<String> data) {
+        List<String> setBits = data.stream().filter((s -> s.charAt(index) == '1')).collect(Collectors.toList());
+        List<String> unsetBits = data.stream().filter((s -> s.charAt(index) == '0')).collect(Collectors.toList());
+
+        if (pred.test(setBits, unsetBits)) {
+            return setBits;
         }
+        return unsetBits;
     }
 
-    private void setRates() {
-        int len = binaryData.size();
-        int mask = (int)Math.pow(2, 11);
-        int i = 11;
-        for (; i >= 0; mask >>= 1)  {
-            int amount = amountSet[i--];
-            if (amount > len - amount) {
-                gammaRate |= mask;
-            } else {
-                epsilonRate |= mask;
+    /*
+     * To find oxygen generator rating, determine the most common value (0 or 1) in the current bit position,
+     * and keep only numbers with that bit in that position.
+     * If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+     */
+    private String findO2Rating() {
+        BiPredicate<List, List> mostCommonFactor = new BiPredicate<List, List>() {
+            @Override
+            public boolean test(List list, List otherList) {
+                return list.size() >= otherList.size();
             }
+        };
+
+
+        List<String> leftovers = new ArrayList<>(binaryData);
+        for (int i = 0; i < 12; i++) {
+            leftovers = getCommonListOnPredicate(i, mostCommonFactor, leftovers);
+            if (leftovers.size() == 1) break;
         }
+        return leftovers.get(0);
     }
 
-    public int getGammaByEpsilon() {
-        return gammaRate * epsilonRate;
+    /*
+    * To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit position,
+    * and keep only numbers with that bit in that position.
+    * If 0 and 1 are equally common, keep values with a 0 in the position being considered.
+    * */
+    private String findCO2ScrubbingRating() {
+        BiPredicate<List, List> leastCommonFactor = (list, otherList) -> list.size() < otherList.size();
+
+        List<String> leftovers = new ArrayList<>(binaryData);
+        for (int i = 0; i < 12; i++) {
+            leftovers = getCommonListOnPredicate(i, leastCommonFactor, leftovers);
+            if (leftovers.size() == 1) break;
+        }
+        return leftovers.get(0);
     }
 
     public static void main(String[] args) {
@@ -55,7 +81,7 @@ public class Day3Part2 {
             Day3Part2 challenge = new Day3Part2(input.lines().collect(Collectors.toList()));
             challenge.solve();
 
-
+            System.out.println(challenge.getLifeSupportRating());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
