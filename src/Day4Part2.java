edd5defacffaf259d69baf2d574d839a76c51b36
t <loim988@gmail.com>
2021-12-05 23:51:46 -0700

    day4 solns

--- src/Day4Part2.java
+++ src/Day4Part2.java
@@ -1,20 +1,17 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class Day4 {
+public class Day4Part2 {
     List<Integer> gameData;
     List<BingoBoard> bingoBoards;
 
-    Day4(List<String> data) {
+    Day4Part2(List<String> data) {
         bingoBoards = new ArrayList<>();
         this.gameData = Arrays.stream(data.get(0).split(",")).mapToInt(Integer::parseInt)
                 .boxed()
@@ -42,19 +39,20 @@ public class Day4 {
     }
 
     public int solve() {
-        boolean won = false;
         BingoBoard currentBoard = null;
+        BingoBoard lastRemoved;
         int call;
         do {
             call = gameData.remove(0);
-            for (BingoBoard bingoBoard : bingoBoards) {
-                currentBoard = bingoBoard;
+            Iterator<BingoBoard> it = bingoBoards.iterator();
+            while (it.hasNext()) {
+                currentBoard = it.next();
                 currentBoard.mark(call);
-                if ((won = currentBoard.checkIfWon())) {
-                    break;
+                if (currentBoard.checkIfWon()) {
+                    it.remove();
                 }
             }
-        } while (!won);
+        } while (!bingoBoards.isEmpty());
         try {
             return getSolution(currentBoard, call);
         } catch(NullPointerException e) {
@@ -82,70 +80,10 @@ public class Day4 {
         BufferedReader input = null;
         try {
             input = new BufferedReader(new FileReader("./data/Day4.txt"));
-            Day4 challenge = new Day4(input.lines().collect(Collectors.toList()));
+            Day4Part2 challenge = new Day4Part2(input.lines().collect(Collectors.toList()));
             System.out.println(challenge.solve());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-    }
-}
-
-class BingoBoard {
-    final private List<List<Integer>> board;
-    final int MARK = -1;
-    final int WIN_CONDITION = -5;
-    public boolean won;
-
-    BingoBoard(List<List<Integer>> numbers) {
-        this.board = numbers;
-    }
-
-    public void mark(int calledNumber) {
-        for (List<Integer> line: board) {
-            if (line.contains(calledNumber)) {
-                line.set(line.indexOf(calledNumber), MARK);
-            }
-        }
-    }
-
-    public boolean checkIfWon() {
-        won = checkColumns() || checkRows();
-        return won;
-    }
-
-    public boolean checkRows() {
-        for (List<Integer> line : board) {
-            if (line.stream().reduce(0, Integer::sum) == WIN_CONDITION) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean checkColumns() {
-        int sum = 0;
-        for (int col = 0; col < 5; col++) {
-            for (List<Integer> line : board) {
-                sum += line.get(col);
-            }
-
-            if (sum == WIN_CONDITION) return true;
-            sum = 0;
-        }
-        return false;
-    }
-
-    public void normalizeBoard() {
-        for (List<Integer> line : board) {
-            for (int i = 0; i < line.size(); i++) {
-                if (line.get(i) == MARK) {
-                    line.set(i, 0);
-                }
-            }
-        }
-    }
-
-    public List<List<Integer>> getBoard() {
-        return board;
     }
 }
